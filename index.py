b4f1d53e371d9940977fc3da89f658ebc76679c0
mohammed <67878024+mohammed-sharaki@users.noreply.github.com>
2021-01-02 08:07:12 -0900

    Update index.py

--- index.py
+++ index.py
@@ -1,36 +1,60 @@
-  
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-import sys
-
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.uic import loadUiType
-#import pafy
-#import humanize
-
-import os
+import urllib.request
+import os 
 from os import path
-
-
+import sys
 FROM_CLASS,_=loadUiType(path.join(path.dirname(__file__),"main.ui"))
-class MainApp(QMainWindow , FROM_CLASS):
-    def __init__(self , parent=None):
-        super(MainApp , self).__init__(parent)
+
+class mainapp(QMainWindow,FROM_CLASS):
+    def __init__(self,parent=None):
+        super(mainapp,self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
- 
+        self.handel_ui()
+        self.Download()
+        self.handel_button()
+    
+    
+    
+    
+    
+    def handel_ui(self):
+        self.setWindowTitle("PyDownload")
+        self.setFixedSize(684,299)
+    def handel_button(self):
+        self.pushButton.clicked.connect(self.download)
+
+    def handel_browse(self):
+        pass
+
+    def Handel_Progress(self, blocknum, blocksize,totalsize):
+        pass
 
 
+    def Download(self):
+        url = self.lineEdit.text()
+        save_location = self.lineEdit_2.text()
 
+        try:
+            urllib.request.urlretrieve(url,save_location,self.Handel_Progress)
+        except Exception:
+            QMessageBox.warning(self, "Download Error", "Download Failed")
+            return
 
+        QMessageBox.information(self,"Download Completed","Download Finished")
 
+        self.progressBar.setValue(0)
+        self.self.lineEdit.setText("")
+        self.self.lineEdit_2.setText("")
 
+def main(): 
+    app=QApplication(sys.argv)
+    window=mainapp()
+    window.show()
+    app.exec_()
 
-def main(): #to start app 
-    app = QApplication(sys.argv)
-    window = MainApp() 
-    window.show() #to show app
-    app.exec_() #infent loop  
-
-if __name__ == '__main__':
+if __name__=="__main__":
     main()
